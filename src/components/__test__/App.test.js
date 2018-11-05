4112149678cbc818042b77e8d1430cde969014b5
Henry Lee <adria.0190@gmail.com>
2018-11-05 20:33:20 +1100

    add comment

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('App component - Shows a comment box', () => {
+    //JSDOM simulate the browser
     const div = document.createElement('div');
-
+    
     ReactDOM.render(<App />, div);
 
     //see if the commentbox is in there
