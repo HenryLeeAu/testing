947e5d57d6b1d04170e71b9426642d3a654576af
Henry Lee <adria.0190@gmail.com>
2018-11-05 20:29:53 +1100

    create base testing

---
+++ src/components/__test__/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+it('App component - Shows a comment box', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App />, div);
+
+    //see if the commentbox is in there
+    expect(div.innerHTML).toContain('comment box')
+
+    //clear memory
+    ReactDOM.unmountComponentAtNode(div);
+})
